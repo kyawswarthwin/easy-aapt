c6940e885a552ba5134107aea17d401ccab80a72
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2018-08-14 04:48:02 +0630

    support xapk

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const mime = require('mime-types');
 const yauzl = require('yauzl');
 
 function packageInfo(filePath, aapt = path.join(__dirname, 'bin', os.platform(), 'aapt')) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const ext = path.extname(filePath).toLowerCase();
+      switch (ext) {
+        case '.apk':
+          resolve(await apkInfo(filePath, aapt));
+          break;
+        case '.xapk':
+          resolve(await xapkInfo(filePath));
+          break;
+        default:
+          throw 'Error: Unsupported File Format';
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function apkInfo(filePath, aapt) {
   return new Promise((resolve, reject) => {
     exec(`"${aapt}" d badging "${filePath}"`, (err, stdout, stderr) => {
       const error = err || stderr;
@@ -33,7 +53,57 @@ function packageInfo(filePath, aapt = path.join(__dirname, 'bin', os.platform(),
   });
 }
 
-function getIcon(filePath, fileName) {
+function xapkInfo(filePath) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const manifest = await getManifest(filePath);
+      const icon = await getIcon(filePath);
+      resolve({
+        name: manifest.name,
+        icon: icon,
+        packageName: manifest.package_name,
+        versionCode: Number(manifest.version_code),
+        versionName: manifest.version_name,
+        requiredSdk: Number(manifest.min_sdk_version)
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function getManifest(filePath) {
+  return new Promise((resolve, reject) => {
+    yauzl.open(filePath, { lazyEntries: true }, (err, zipfile) => {
+      if (err) {
+        reject(err);
+      } else {
+        zipfile.readEntry();
+        zipfile.on('entry', entry => {
+          if (entry.fileName === 'manifest.json') {
+            zipfile.openReadStream(entry, (err, stream) => {
+              if (err) {
+                reject(err);
+              } else {
+                const buf = [];
+                stream.on('data', chunk => {
+                  buf.push(chunk);
+                });
+                stream.on('end', () => {
+                  const manifest = JSON.parse(Buffer.concat(buf).toString());
+                  resolve(manifest);
+                });
+              }
+            });
+          }
+          zipfile.readEntry();
+        });
+      }
+    });
+  });
+}
+
+function getIcon(filePath, fileName = 'icon.png') {
   return new Promise((resolve, reject) => {
     yauzl.open(filePath, { lazyEntries: true }, (err, zipfile) => {
       if (err) {
